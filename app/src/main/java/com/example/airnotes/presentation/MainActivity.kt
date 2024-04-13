afdc3ba4b4bdce4d104585ee3877a5349a2dccce
Sergei Plekhanov <sergeyplehanov2014@yandex.ru>
2024-04-13 14:18:30 +0300

    Bottom sheet added

--- app/src/main/java/com/example/airnotes/presentation/MainActivity.kt
+++ app/src/main/java/com/example/airnotes/presentation/MainActivity.kt
@@ -12,6 +12,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+//        AppCompatDelegate.MODE_NIGHT_YES
         binding = MainActivityBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
